0d770b0d713dcc5e2e95c0be8b4a8ebb0c4f3b41
dekguh <kdkteguhmahesa@gmail.com>
2023-12-20 08:54:29 +0800

    feat: sign out button

--- src/features/overview/routes/Overview.tsx
+++ src/features/overview/routes/Overview.tsx
@@ -1,6 +1,22 @@
+import { updateIsLogged } from '@/features/auth/stores/authSlice'
+import { clearStorageJwt } from '@/utils/storage'
+import { useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+
 const Overview = () => {
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  const handleLogoutClick = () => {
+    clearStorageJwt()
+    dispatch(updateIsLogged(false))
+    navigate('/auth/sign-in')
+  }
+
   return (
-    <div>Overview</div>
+    <div>
+      <button onClick={handleLogoutClick}>logout</button>
+    </div>
   )
 }
 
