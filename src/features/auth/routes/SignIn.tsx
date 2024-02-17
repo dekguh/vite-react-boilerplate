1436f39798e11f20bee7d8dc4962fb182b25584b
dekguh <kdkteguhmahesa@gmail.com>
2024-02-17 11:34:20 +0800

    feat: example using rtk query

--- src/features/auth/routes/SignIn.tsx
+++ src/features/auth/routes/SignIn.tsx
@@ -2,20 +2,31 @@ import { setStorageJwt } from '@/utils/storage'
 import { useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import { updateIsLogged } from '../stores/authSlice'
+import { usePostSignInMutation } from '../api/authApi'
 
 export const SignIn = () => {
   const navigate = useNavigate()
   const dispatch = useDispatch()
+  const [signInUser] = usePostSignInMutation()
 
-  const handleLoginClick = () => {
-    setStorageJwt('logged')
-    dispatch(updateIsLogged(true))
-    navigate('/')
+  const handleLoginClick = async () => {
+    try {
+      const response = await signInUser({
+        'email': 'eve.holt@reqres.in',
+        'password': 'cityslicka'
+      }).unwrap()
+  
+      setStorageJwt(response?.token)
+      dispatch(updateIsLogged(true))
+      navigate('/')
+    } catch (error) {
+      // handle error
+    }
   }
 
   return (
     <div>
-      <button data-testid='btnLoginId' onClick={handleLoginClick}>login</button>
+      <button data-testid='btnLoginId' onClick={() => handleLoginClick()}>login</button>
     </div>
   )
 }
