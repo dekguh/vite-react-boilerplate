b521aa5c027ae4f64d8bd271782e170f2fe7b84b
dekguh <kdkteguhmahesa@gmail.com>
2024-05-10 13:16:07 +0800

    chore: setup eslint perfectionist for structure import naming

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,10 +1,11 @@
+/* eslint-disable no-undef */
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', 'plugin:storybook/recommended'],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'perfectionist'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
@@ -19,6 +20,47 @@ module.exports = {
     'jsx-quotes': ['error', 'prefer-single'],
     'semi': ['error', 'never'],
     'indent': ['error', 2],
-    'quotes': ['error', 'single']
+    'quotes': ['error', 'single'],
+    'perfectionist/sort-interfaces': 'error',
+    'perfectionist/sort-imports': [
+      'error',
+      {
+        type: 'line-length',
+        order: 'asc',
+        'groups': [
+          'react',
+          'internal-components',
+          'custom-components',
+          'custom-config',
+          'custom-features',
+          'custom-hooks',
+          'custom-lib',
+          'custom-providers',
+          'custom-routes',
+          'custom-stores',
+          'custom-types',
+          'custom-utils'
+        ],
+        'custom-groups': {
+          value: {
+            'react': ['react', 'react-dom/*', 'react-redux', 'react-router-dom', '@reduxjs/**'],
+            'internal-components': './**',
+            'custom-components': '@/components/**',
+            'custom-config': '@/config/**',
+            'custom-features': '@/features/**',
+            'custom-hooks': '@/hooks/**',
+            'custom-lib': '@/lib/**',
+            'custom-providers': '@/providers/**',
+            'custom-routes': '@/routes/**',
+            'custom-stores': '@/stores/**',
+            'custom-types': '@/types/**',
+            'custom-utils': '@/utils/**'
+          }
+        },
+        'internal-pattern': [
+          '@/**'
+        ]
+      }
+    ]
   },
 }
